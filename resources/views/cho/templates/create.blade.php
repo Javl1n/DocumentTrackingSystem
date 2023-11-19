23ccac591e893af501f47b1906e0235cf174977a
Javl1n <farmodia@gmail.com>
2023-11-20 02:11:51 +0800

    added description field

--- resources/views/cho/templates/create.blade.php
+++ resources/views/cho/templates/create.blade.php
@@ -6,11 +6,20 @@
     </x-slot>
 
     <x-panel class="flex-auto w-2/4 mx-auto">
-        <form action="/cho/templates" method="post" enctype="multipart/form-data">
+        <form action="/cho/templates" method="post" enctype="multipart/form-data" class="flex flex-col h-full">
             @csrf
             <x-form.text-input-group name="title" type="text" />
             <x-form.text-input-group name="cycle" type="number" label="Update Cycle" />
             <x-form.text-input-group name="link" type="file" label="File" />
+            <div class="mt-4 flex-1">
+                <x-input-label for="description" :value="__('Description')" />
+                <textarea 
+                    name="description" 
+                    id="description" 
+                    class="w-full h-full mt-1 bg-gray-900 resize-none border-slate-700 rounded-lg"
+                ></textarea>                
+                <x-input-error :messages="$errors->get('description')" class="mt-2" />
+            </div>
             <div class="flex justify-end">
                 <x-primary-button class="mt-10">
                     {{ __('Submit') }}
